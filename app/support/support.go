21e80d8e1f095f2d0154ad34bbde57ccaca4724d
Anton Novojilov <andy@essentialkaos.com>
2023-08-13 12:05:48 +0300

    Improve verbose version info gathering

--- app/support/support.go
+++ app/support/support.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"runtime/debug"
 	"strings"
 
 	"github.com/essentialkaos/ek/v12/fmtc"
@@ -54,6 +55,10 @@ func showApplicationInfo(app, ver, gitRev string) {
 		strings.TrimLeft(runtime.Version(), "go"),
 		runtime.GOOS, runtime.GOARCH,
 	))
+
+	if gitRev == "" {
+		gitRev = extractGitRevFromBuildInfo()
+	}
 
 	if gitRev != "" {
 		if !fmtc.DisableColors && fmtc.IsTrueColorSupported() {
@@ -140,6 +145,23 @@ func showDepsInfo(gomod []byte) {
 			fmtc.Printf(" {s}%8s{!}  %s {s-}(%s){!}\n", dep.Version, dep.Path, dep.Extra)
 		}
 	}
+}
+
+// extractGitRevFromBuildInfo extracts git SHA from embedded build info
+func extractGitRevFromBuildInfo() string {
+	info, ok := debug.ReadBuildInfo()
+
+	if !ok {
+		return ""
+	}
+
+	for _, s := range info.Settings {
+		if s.Key == "vcs.revision" && len(s.Value) > 7 {
+			return s.Value[:7]
+		}
+	}
+
+	return ""
 }
 
 // getHashColorBullet return bullet with color from hash
--- go.mod
+++ go.mod
@@ -13,6 +13,6 @@ require (
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/pierrec/lz4/v4 v4.1.18 // indirect
 	github.com/ulikunitz/xz v0.5.11 // indirect
-	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
